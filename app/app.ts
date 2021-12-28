c2bea594889287e51b2a9df59e112dea1b29ead0
guijun13 <guilhermejun13.grillo@gmail.com>
2021-12-28 15:58:16 -0300

    feat: add negociacao controllers

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,14 @@
-import { Negociacao } from './models/negociacao.js';
+// import { Negociacao } from './models/negociacao.js';
 
-const negociacao = new Negociacao(new Date(), 10, 100);
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
 
-console.log(negociacao.volume);
+// const negociacao = new Negociacao(new Date(), 10, 100);
+
+// console.log(negociacao.volume);
+
+const controller = new NegociacaoController();
+const form = document.querySelector(".form");
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  controller.adiciona();
+})
