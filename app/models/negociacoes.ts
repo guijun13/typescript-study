d2c8508c6cb77c3bcd4cd59f82020a39a042e826
guijun13 <guilhermejun13.grillo@gmail.com>
2021-12-29 00:03:04 -0300

    feat: add new declaration for arrays using []

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,15 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes{
-  private negociacoes: Array<Negociacao> = [];
+  // private negociacoes: Array<Negociacao> = [];
+  private negociacoes: Negociacao[] = [];
 
   adiciona(negociacao: Negociacao){
     this.negociacoes.push(negociacao);
   }
 
-  listaNegociacoes(): ReadonlyArray<Negociacao>{ // dont allow edit array functions (e.g. pop())
+  // listaNegociacoes(): ReadonlyArray<Negociacao>{ // dont allow edit array functions (e.g. pop())
+  listaNegociacoes(): readonly Negociacao[]{
     return this.negociacoes;
   }
 }
