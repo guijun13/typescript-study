199bdb7e13f5582527534e25903d85da8bcc8ba9
guijun13 <guilhermejun13.grillo@gmail.com>
2022-01-03 19:25:49 -0300

    feat(controller): block addition on sat and sun

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -10,6 +10,8 @@ export class NegociacaoController{
   private negociacoes: Negociacoes =  new Negociacoes();
   private negociacoesView: NegociacoesView = new NegociacoesView('#negociacoesView');
   private mensagemView: MensagemView = new MensagemView('#mensagemView');
+  private readonly SABADO = 6;
+  private readonly DOMINGO = 0;
 
   constructor(){
     this.inputData = document.querySelector('#data');
@@ -20,9 +22,19 @@ export class NegociacaoController{
 
   public adiciona() : void{
     const negociacao = this.criaNegociacao();
+
+    if(!this.ehDiaUtil(negociacao.data)){
+      this.mensagemView.update('Apenas negociações em dias úteis são aceitas');
+      return;
+    }
+
     this.negociacoes.adiciona(negociacao);
     this.limparFormulario();
     this.atualizaView();
+  }
+
+  private ehDiaUtil(data: Date): Boolean {
+    return data.getDay() > this.DOMINGO && data.getDay() < this.SABADO;
   }
 
   private criaNegociacao(): Negociacao{
