cdd176494aed94b90ecd9d9d18504203c0eba897
guijun13 <guilhermejun13.grillo@gmail.com>
2022-01-04 23:57:16 -0300

    feat(negociacao): create static method criaDe
    same func of old criaNegociacao()

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -39,4 +39,12 @@ export class Negociacao {
   get volume(): number {
     return this._quantidade * this._valor;
   }
+
+  public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao{ // metodo static: pode ser chamado diretamento da classe (Negociacao)
+    const exp = /-/g;
+    const data = new Date(dataString.replace(exp, ','));
+    const quantidade = parseInt(quantidadeString);
+    const valor = parseFloat(valorString);
+    return new Negociacao(data, quantidade, valor);
+  }
 }
